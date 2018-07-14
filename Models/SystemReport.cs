9ca7fcfbc63e9dd7b9e97b06d4400f016391390d
dtwilliams10 <david.williams110913@gmail.com>
2018-07-14 15:05:14 -0400

    Project startup

---
+++ Models/SystemReport.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LHMSAPI.Models
+{
+    public class SystemReport
+    {
+        
+        public ObjectId ObjectID {get; set;}
+        
+        [BsonElement("ReportID")]
+        public int ReportID {get; set;}
+
+        [BsonElement("Name")]
+        public string Name {get; set;}
+
+        [DataType(DataType.Date)]
+        DateTime ReportDate {get; set;}
+
+        [Required]
+        SystemName SystemName {get; set;}
+
+        [Required]
+        string SystemUpdate {get; set;}
+
+        [Required]
+        string PersonnelUpdates {get; set;}
+
+        [Required]
+        string CreativeIdeasAndEvaluations {get; set;}
+
+        [Required]
+        string BarriersOrChallenges {get; set;}
+
+        [Required]
+        string HowCanIHelpYou {get; set;}
+
+        [Required]
+        string PersonalGrowthAndDevelopment {get; set;}
+    }
+
+    public enum SystemName {
+        Youth,
+        FirstTouch,
+        SecondTouch,
+        SpecializedMinistries,
+        Childrens,
+        Administrative,
+        CampusPreservation,
+        ChristianDevelopment
+    }
+}
