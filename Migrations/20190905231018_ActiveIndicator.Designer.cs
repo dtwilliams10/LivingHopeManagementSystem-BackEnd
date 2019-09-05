6e9f5e22da45dd03f3c0a8f639761f2a5f686bda
dtwilliams10 <david.williams110913@gmail.com>
2019-09-05 19:34:39 -0400

    Added System Report Status and Active Indicator

--- Migrations/20190905231018_ActiveIndicator.Designer.cs
+++ Migrations/20190905231018_ActiveIndicator.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using LHMSAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace LHMSAPI.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190905231018_ActiveIndicator")]
+    partial class ActiveIndicator
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/SystemReportModel.cs
+++ Models/SystemReportModel.cs
@@ -1,39 +1,52 @@
 public class SystemReport
-    {
-        public int Id { get; set; }
+{
+    public int Id { get; set; }
 
-        //TODO: Update with logic to use the logged in user's info from Identity Server 4.
-        public string Name { get; set; }
+    //TODO: Update with logic to use the logged in user's info from Identity Server 4.
+    //TODO: Need to add a status indicatory whether the record is active or not.
+    //TODO: Need to add a report status to show where in the lifecycle it is.
+    public string Name { get; set; }
 
-        public System.DateTime ReportDate { get; set; }
+    public bool Active { get; set; }
+    public System.DateTime ReportDate { get; set; }
 
-        public System.DateTime CreatedDate { get; set; }
+    public System.DateTime CreatedDate { get; set; }
 
-        public System.DateTime UpdatedDate { get; set; } = System.DateTime.Now;
+    public System.DateTime UpdatedDate { get; set; } = System.DateTime.Now;
 
-        public SystemName SystemName { get; set; }
+    public SystemReportStatus SystemReportStatus { get; set; }
 
-        public string SystemUpdate { get; set; }
+    public SystemName SystemName { get; set; }
 
-        public string PersonnelUpdates { get; set; }
+    public string SystemUpdate { get; set; }
 
-        public string CreativeIdeasAndEvaluations { get; set; }
+    public string PersonnelUpdates { get; set; }
 
-        public string BarriersOrChallenges { get; set; }
+    public string CreativeIdeasAndEvaluations { get; set; }
 
-        public string HowCanIHelpYou { get; set; }
+    public string BarriersOrChallenges { get; set; }
 
-        public string PersonalGrowthAndDevelopment { get; set; }
-    }
+    public string HowCanIHelpYou { get; set; }
 
-    public enum SystemName
-    {
-        Youth,
-        FirstTouch,
-        SecondTouch,
-        SpecializedMinistries,
-        Childrens,
-        Administrative,
-        CampusPreservation,
-        ChristianDevelopment
-    }
+    public string PersonalGrowthAndDevelopment { get; set; }
+}
+
+public enum SystemName
+{
+    Youth,
+    FirstTouch,
+    SecondTouch,
+    SpecializedMinistries,
+    Childrens,
+    Administrative,
+    CampusPreservation,
+    ChristianDevelopment
+}
+
+public enum SystemReportStatus
+{
+    Draft,
+    SubmittedToSystemDirector,
+    SubmittedToAdministrativePastor,
+    Canceled
+}
