3ff4498948381577de733b375270af1109d29cc5
Tyler Williams <david.williams110913@gmail.com>
2019-09-18 11:51:52 +0000

    Fixed typos and formatting. Removed completed comments.

--- Models/SystemReportModel.cs
+++ Models/SystemReportModel.cs
@@ -3,11 +3,11 @@ public class SystemReport
     public int Id { get; set; }
 
     //TODO: Update with logic to use the logged in user's info from Identity Server 4.
-    //TODO: Need to add a status indicatory whether the record is active or not.
-    //TODO: Need to add a report status to show where in the lifecycle it is.
+    
     public string Name { get; set; }
 
     public bool Active { get; set; }
+    
     public System.DateTime ReportDate { get; set; }
 
     public System.DateTime CreatedDate { get; set; }
@@ -48,5 +48,5 @@ public enum SystemReportStatus
     Draft,
     SubmittedToSystemDirector,
     SubmittedToAdministrativePastor,
-    Canceled
+    Cancelled
 }
