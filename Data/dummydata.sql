3054ca4cf93f624b8473dd53332e3e88ab066ac9
dtwilliams10 <david.williams110913@gmail.com>
2019-11-07 20:36:17 -0500

    Updated to run correctly on all servers

--- Data/dummydata.sql
+++ Data/dummydata.sql
@@ -1,17 +1,135 @@
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+VALUES (1,
+'Administrative'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+2,
+'Childrens'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+3,
+'First Touch'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+4,
+'Second Touch'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+5,
+'Youth'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+6,
+'Worship'
+);
+
+INSERT INTO public."SystemName"(
+"Id",
+"Name"
+)
+
+VALUES (
+7,
+'Administrative'
+);
+
+INSERT INTO public."SystemStatus"(
+	"Id",
+	"Status"
+)
+
+VALUES (
+	0,
+	'Inactive'
+);
+
+INSERT INTO public."SystemStatus"(
+	"Id",
+	"Status"
+)
+
+VALUES (
+	1,
+	'Draft'
+);
+
+INSERT INTO public."SystemStatus"(
+	"Id",
+	"Status"
+)
+
+VALUES (
+	2,
+	'Submitted'
+);
+
+INSERT INTO public."SystemStatus"(
+	"Id",
+	"Status"
+)
+
+VALUES (
+	3,
+	'Approved'
+);
+
+INSERT INTO public."SystemStatus"(
+	"Id",
+	"Status"
+)
+
+VALUES (
+	4,
+	'Rejected'
+);
+
 INSERT INTO public."SystemReports"(
 "Id",
 "Name",
 "ReportDate", 
 "CreatedDate", 
 "UpdatedDate",
-"SystemName",
+"SystemNameId",
 "SystemUpdate",
 "PersonnelUpdates",
 "CreativeIdeasAndEvaluations",
 "BarriersOrChallenges",
 "HowCanIHelpYou",
 "PersonalGrowthAndDevelopment",
-"SystemReportStatus",
+"SystemReportStatusId",
 "Active")
 VALUES (1,
 'Tyler Williams',
@@ -25,7 +143,7 @@ VALUES (1,
 'These are my barriers or challenges',
 'this is how you can help me',
 'Personal Growth and Development',
-'0',
+0,
 '1');
 
 
@@ -34,14 +152,14 @@ INSERT INTO public."SystemReports"("Id",
 "ReportDate",
 "CreatedDate",
 "UpdatedDate",
-"SystemName",
+"SystemNameId",
 "SystemUpdate",
 "PersonnelUpdates",
 "CreativeIdeasAndEvaluations",
 "BarriersOrChallenges",
 "HowCanIHelpYou",
 "PersonalGrowthAndDevelopment",
-"SystemReportStatus",
+"SystemReportStatusId",
 "Active")
 VALUES (2,
 'Tyler Williams',
@@ -55,7 +173,36 @@ VALUES (2,
 'These are my barriers or challenges',
 'this is how you can help me',
 'Personal Growth and Development',
-'0',
+0,
+'1');
+
+INSERT INTO public."SystemReports"("Id",
+"Name",
+"ReportDate",
+"CreatedDate",
+"UpdatedDate",
+"SystemNameId",
+"SystemUpdate",
+"PersonnelUpdates",
+"CreativeIdeasAndEvaluations",
+"BarriersOrChallenges",
+"HowCanIHelpYou",
+"PersonalGrowthAndDevelopment",
+"SystemReportStatusId",
+"Active")
+VALUES (3,
+'Tyler Williams',
+'today',
+'today',
+'today',
+1,
+'This is my System Update',
+'This is my personnel Update',
+'These are my Creative Ideas and Evaluations',
+'These are my barriers or challenges',
+'this is how you can help me',
+'Personal Growth and Development',
+0,
 '1');
 
 
@@ -64,146 +211,116 @@ INSERT INTO public."SystemReports"("Id",
 "ReportDate",
 "CreatedDate",
 "UpdatedDate",
-"SystemName",
+"SystemNameId",
 "SystemUpdate",
 "PersonnelUpdates",
 "CreativeIdeasAndEvaluations",
 "BarriersOrChallenges",
 "HowCanIHelpYou",
 "PersonalGrowthAndDevelopment",
-"SystemReportStatus",
+"SystemReportStatusId",
 "Active")
-VALUES (3,
-									'Tyler Williams',
-									'today',
-									'today',
-									'today',
-									1,
-									'This is my System Update',
-									'This is my personnel Update',
-									'These are my Creative Ideas and Evaluations',
-									'These are my barriers or challenges',
-									'this is how you can help me',
-									'Personal Growth and Development',
-									'0',
-									'1');
-
-
-INSERT INTO public."SystemReports"("Id",
-																																				"Name",
-																																				"ReportDate",
-																																				"CreatedDate",
-																																				"UpdatedDate",
-																																				"SystemName",
-																																				"SystemUpdate",
-																																				"PersonnelUpdates",
-																																				"CreativeIdeasAndEvaluations",
-																																				"BarriersOrChallenges",
-																																				"HowCanIHelpYou",
-																																				"PersonalGrowthAndDevelopment",
-																																				"SystemReportStatus",
-																																				"Active")
 VALUES (4,
-									'Tyler Williams',
-									'today',
-									'today',
-									'today',
-									1,
-									'This is my System Update',
-									'This is my personnel Update',
-									'These are my Creative Ideas and Evaluations',
-									'These are my barriers or challenges',
-									'this is how you can help me',
-									'Personal Growth and Development',
-									'0',
-									'1');
+'Tyler Williams',
+'today',
+'today',
+'today',
+1,
+'This is my System Update',
+'This is my personnel Update',
+'These are my Creative Ideas and Evaluations',
+'These are my barriers or challenges',
+'this is how you can help me',
+'Personal Growth and Development',
+0,
+'1');
 
 
 INSERT INTO public."SystemReports"("Id",
-																																				"Name",
-																																				"ReportDate",
-																																				"CreatedDate",
-																																				"UpdatedDate",
-																																				"SystemName",
-																																				"SystemUpdate",
-																																				"PersonnelUpdates",
-																																				"CreativeIdeasAndEvaluations",
-																																				"BarriersOrChallenges",
-																																				"HowCanIHelpYou",
-																																				"PersonalGrowthAndDevelopment",
-																																				"SystemReportStatus",
-																																				"Active")
+"Name",
+"ReportDate",
+"CreatedDate",
+"UpdatedDate",
+"SystemNameId",
+"SystemUpdate",
+"PersonnelUpdates",
+"CreativeIdeasAndEvaluations",
+"BarriersOrChallenges",
+"HowCanIHelpYou",
+"PersonalGrowthAndDevelopment",
+"SystemReportStatusId",
+"Active")
 VALUES (5,
-									'Tyler Williams',
-									'today',
-									'today',
-									'today',
-									1,
-									'This is my System Update',
-									'This is my personnel Update',
-									'These are my Creative Ideas and Evaluations',
-									'These are my barriers or challenges',
-									'this is how you can help me',
-									'Personal Growth and Development',
-									'0',
-									'1');
+'Tyler Williams',
+'today',
+'today',
+'today',
+1,
+'This is my System Update',
+'This is my personnel Update',
+'These are my Creative Ideas and Evaluations',
+'These are my barriers or challenges',
+'this is how you can help me',
+'Personal Growth and Development',
+0,
+'1');
 
 
 INSERT INTO public."SystemReports"("Id",
-																																				"Name",
-																																				"ReportDate",
-																																				"CreatedDate",
-																																				"UpdatedDate",
-																																				"SystemName",
-																																				"SystemUpdate",
-																																				"PersonnelUpdates",
-																																				"CreativeIdeasAndEvaluations",
-																																				"BarriersOrChallenges",
-																																				"HowCanIHelpYou",
-																																				"PersonalGrowthAndDevelopment",
-																																				"SystemReportStatus",
-																																				"Active")
+"Name",
+"ReportDate",
+"CreatedDate",
+"UpdatedDate",
+"SystemNameId",
+"SystemUpdate",
+"PersonnelUpdates",
+"CreativeIdeasAndEvaluations",
+"BarriersOrChallenges",
+"HowCanIHelpYou",
+"PersonalGrowthAndDevelopment",
+"SystemReportStatusId",
+"Active")
 VALUES (6,
-									'Tyler Williams',
-									'today',
-									'today',
-									'today',
-									1,
-									'This is my System Update',
-									'This is my personnel Update',
-									'These are my Creative Ideas and Evaluations',
-									'These are my barriers or challenges',
-									'this is how you can help me',
-									'Personal Growth and Development',
-									'0',
-									'1');
+'Tyler Williams',
+'today',
+'today',
+'today',
+1,
+'This is my System Update',
+'This is my personnel Update',
+'These are my Creative Ideas and Evaluations',
+'These are my barriers or challenges',
+'this is how you can help me',
+'Personal Growth and Development',
+0,
+'1');
 
 
 INSERT INTO public."SystemReports"("Id",
-																																				"Name",
-																																				"ReportDate",
-																																				"CreatedDate",
-																																				"UpdatedDate",
-																																				"SystemName",
-																																				"SystemUpdate",
-																																				"PersonnelUpdates",
-																																				"CreativeIdeasAndEvaluations",
-																																				"BarriersOrChallenges",
-																																				"HowCanIHelpYou",
-																																				"PersonalGrowthAndDevelopment",
-																																				"SystemReportStatus",
-																																				"Active")
+"Name",
+"ReportDate",
+"CreatedDate",
+"UpdatedDate",
+"SystemNameId",
+"SystemUpdate",
+"PersonnelUpdates",
+"CreativeIdeasAndEvaluations",
+"BarriersOrChallenges",
+"HowCanIHelpYou",
+"PersonalGrowthAndDevelopment",
+"SystemReportStatusId",
+"Active")
 VALUES (7,
-									'Tyler Williams',
-									'today',
-									'today',
-									'today',
-									1,
-									'This is my System Update',
-									'This is my personnel Update',
-									'These are my Creative Ideas and Evaluations',
-									'These are my barriers or challenges',
-									'this is how you can help me',
-									'Personal Growth and Development',
-									'0',
-									'1');
+'Tyler Williams',
+'today',
+'today',
+'today',
+1,
+'This is my System Update',
+'This is my personnel Update',
+'These are my Creative Ideas and Evaluations',
+'These are my barriers or challenges',
+'this is how you can help me',
+'Personal Growth and Development',
+1,
+'1');
