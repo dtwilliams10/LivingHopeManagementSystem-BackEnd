5509a99d0a5eefadf64bbdd57fd0e56bd7377bf8
dtwilliams10 <david.williams110913@gmail.com>
2020-01-24 16:47:20 -0500

    Removed .net core identity setup instructions

--- Startup.cs
+++ Startup.cs
@@ -17,60 +17,18 @@ namespace LHMSAPI
             Configuration = configuration;
         }
 
-        protected enum environment { DEV, STAGING, PRODUCTION }
-        protected environment envmnt {get; set; }
-
         readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<StatusRepository>();
-            services.Configure<IdentityOptions>(options =>
-            {
-                //Password settings
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 10;
-                options.Password.RequiredUniqueChars = 1;
-
-                //Lockout settings
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.AllowedForNewUsers = true;
-
-                //User settings
-                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890-._@+";
-                options.User.RequireUniqueEmail = true;
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-                if(envmnt.Equals(environment.PRODUCTION))
-                {
-                    options.LoginPath = "https://lhms.dtwilliams10.com/login";
-                    options.AccessDeniedPath = "https://lhms.dtwillaims10.com/loginDenied";
-                }
-                else if(envmnt.Equals(environment.STAGING))
-                {
-                    options.LoginPath = "https://test.lhms.dtwilliams10.com/login";
-                    options.AccessDeniedPath = "https://test.lhms.dtwillaims10.com/loginDenied";
-                }
-                else if(envmnt.Equals(environment.DEV))
-                {
-                    options.LoginPath = "https://localhost:3000/login";
-                    options.AccessDeniedPath = "https://localhost:3000/loginDenied";
-                }
-                options.SlidingExpiration = true;
-            });
 
             services.AddDbContext<DatabaseContext>(options =>
             {
                 options.UseNpgsql(Configuration.GetConnectionString("PostgreSQL"));
             });
+
             services.AddCors(options =>
             {
                 options.AddPolicy(MyAllowSpecificOrigins,
@@ -82,6 +40,7 @@ namespace LHMSAPI
                                     .SetIsOriginAllowedToAllowWildcardSubdomains();
             });
             });
+
             services.AddControllers();
         }
 
@@ -92,22 +51,10 @@ namespace LHMSAPI
             if (env.IsProduction())
             {
                 app.UseExceptionHandler("/Error");
-                envmnt = environment.PRODUCTION;
-                Log.Information("Environment set to Production");
             }
             else
             {
                 app.UseDeveloperExceptionPage();
-                if (env.IsStaging())
-                {
-                    envmnt = environment.STAGING;
-                    Log.Information("Environment set to Staging");
-                }
-                else if (env.IsDevelopment())
-                {
-                    envmnt = environment.DEV;
-                    Log.Information("Environment set to Development");
-                }
             }
 
             app.UseHttpsRedirection();
