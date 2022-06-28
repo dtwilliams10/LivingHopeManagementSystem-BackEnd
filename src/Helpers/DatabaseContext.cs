7934f57611e2d34b442e70b0cac3f836ad14e5fe
Tyler Williams <david.williams110913@gmail.com>
2022-06-28 10:04:52 -0400

    Changed connection string and added plurality to system names

--- src/Helpers/DatabaseContext.cs
+++ src/Helpers/DatabaseContext.cs
@@ -14,12 +14,12 @@ namespace LHMS.SystemReports.Helpers
         }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseNpgsql(Configuration.GetConnectionString("LHMS"));
+            options.UseNpgsql(Configuration.GetConnectionString("SystemReports"));
         }
 
         public DbSet<SystemReport> SystemReports { get; set; }
         public DbSet<SystemReportStatus> SystemReportStatus { get; set; }
-        public DbSet<SystemName> SystemName { get; set; }
+        public DbSet<SystemName> SystemNames { get; set; }
 
     }
 
