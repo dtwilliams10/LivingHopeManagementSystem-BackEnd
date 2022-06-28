94282a543965b67685c560cb27ebb80c6c92e53b
Tyler Williams <david.williams110913@gmail.com>
2022-06-28 10:05:04 -0400

    Minor whitespace edit

--- src/Models/System Report/SystemReportModel.cs
+++ src/Models/System Report/SystemReportModel.cs
@@ -5,48 +5,48 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace LHMS.SystemReports.Models
 {
     public class SystemReport
-{
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    //TODO: Update with logic to use the logged in user's info from Identity Server 4.
-    public string ReporterName {get; set;}
+        //TODO: Update with logic to use the logged in user's info from Identity Server 4.
+        public string ReporterName { get; set; }
 
-    public string ReportName { get; set; }
+        public string ReportName { get; set; }
 
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime ReportDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReportDate { get; set; }
 
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime CreatedDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
 
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime UpdatedDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime UpdatedDate { get; set; }
 
-    public int SystemReportStatusId { get; set; }
+        public int SystemReportStatusId { get; set; }
 
-    [ForeignKey("SystemReportStatusId")]
-    public SystemReportStatus SystemReportStatus { get; set; }
+        [ForeignKey("SystemReportStatusId")]
+        public SystemReportStatus SystemReportStatus { get; set; }
 
-    public int SystemNameId { get; set; }
+        public int SystemNameId { get; set; }
 
-    [ForeignKey("SystemNameId")]
-    public SystemName SystemName { get; set; }
+        [ForeignKey("SystemNameId")]
+        public SystemName SystemName { get; set; }
 
-    public string SystemUpdate { get; set; }
+        public string SystemUpdate { get; set; }
 
-    public string PersonnelUpdates { get; set; }
+        public string PersonnelUpdates { get; set; }
 
-    public string CreativeIdeasAndEvaluations { get; set; }
+        public string CreativeIdeasAndEvaluations { get; set; }
 
-    public string BarriersOrChallenges { get; set; }
+        public string BarriersOrChallenges { get; set; }
 
-    public string HowCanIHelpYou { get; set; }
+        public string HowCanIHelpYou { get; set; }
 
-    public string PersonalGrowthAndDevelopment { get; set; }
+        public string PersonalGrowthAndDevelopment { get; set; }
 
     }
 
