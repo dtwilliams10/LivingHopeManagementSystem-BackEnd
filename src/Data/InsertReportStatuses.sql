86a236957ede703822a65039066df9a9f5f0212b
Tyler Williams <david.williams110913@gmail.com>
2022-10-27 06:18:27 -0400

    Made insert scripts idempotent

--- src/Data/InsertReportStatuses.sql
+++ src/Data/InsertReportStatuses.sql
@@ -1,14 +1,26 @@
-INSERT INTO system_report_status(status) VALUES ('Inactive');
-INSERT INTO system_report_status(status) VALUES ('Draft');
-INSERT INTO system_report_status(status) VALUES ('Submitted to Team Lead');
-INSERT INTO system_report_status(status) VALUES ('Rejected by Team Lead');
-INSERT INTO system_report_status(status) VALUES ('Approved by Team Lead');
-INSERT INTO system_report_status(status) VALUES ('Submitted to System Drector');
-INSERT INTO system_report_status(status) VALUES ('Approved by System Director');
-INSERT INTO system_report_status(status) VALUES ('Rejected by System Director');
-INSERT INTO system_report_status(status) VALUES ('Submitted to Administrative Pastor');
-INSERT INTO system_report_status(status) VALUES ('Approved by Administrative Pastor');
-INSERT INTO system_report_status(status) VALUES ('Rejected by Administrative Pastor');
-INSERT INTO system_report_status(status) VALUES ('Submitted to Senior Pastor');
-INSERT INTO system_report_status(status) VALUES ('Approved by Senior Pastor');
-INSERT INTO system_report_status(status) VALUES ('Rejected by Senior Pastor');
+--delete from public.system_report_status; -- where id < 15;
+
+select * from system_report_status;
+
+insert into system_report_status
+select *
+from (values (1, 'Inactive'), 
+	  (2, 'Draft'),
+	  (3, 'Submitted to Team Lead'),
+	  (4, 'Rejected by Team Lead'),
+	  (5, 'Approved by Team Lead'),
+	  (6, 'Submitted to System Director'),
+	  (7, 'Rejected by System Director'),
+	  (8, 'Approved by System Director'),
+	  (9, 'Submitted to Administrative Pastor'),
+	  (10, 'Rejected by Administrative Pastor'),
+	  (11, 'Approved by Administrative Pastor'),
+	  (12, 'Submitted to Senior Pastor'),
+	  (13, 'Rejected by Senior Pastor'),
+	  (14, 'Approved by Senior Pastor')
+	  ) as data ("name")
+where not exists (select *
+                  from system_report_status
+                  where data = system_report_status);
+				  
+select * from system_report_status;
--- src/Data/InsertSystemNames.sql
+++ src/Data/InsertSystemNames.sql
@@ -1,12 +1,29 @@
-INSERT INTO system_names(Name) VALUES ('Youth System');
-INSERT INTO system_names(Name) VALUES ('First Touch System');
-INSERT INTO system_names(Name) VALUES ('Second Touch System');
-INSERT INTO system_names(Name) VALUES ('Specialized Ministries System');
-INSERT INTO system_names(Name) VALUES ('Childrens System');
-INSERT INTO system_names(Name) VALUES ('Administrative System');
-INSERT INTO system_names(Name) VALUES ('Campus Preservation System');
-INSERT INTO system_names(Name) VALUES ('Christian Development System');
-INSERT INTO system_names(Name) VALUES ('Hispanic System');
-INSERT INTO system_names(Name) VALUES ('Media System');
-INSERT INTO system_names(Name) VALUES ('Worship System');
-INSERT INTO system_names(Name) VALUES ('Creative System');
+--delete from public.system_names -- where id < 13;
+
+select *
+from system_names;
+
+insert into system_names
+select *
+from (
+        values (1, 'Youth System'),
+            (2, 'First Touch System'),
+            (3, 'Second Touch System'),
+            (4, 'Specialized Ministries System'),
+            (5, 'Childrens System'),
+            (6, 'Administrative System'),
+            (7, 'Campus Preservation System'),
+            (8, 'Christian Development System')
+            (9, 'Hispanic System'),
+            (10, 'Media System'),
+            (11, 'Worship System'),
+            (12, 'Creative System')
+    ) as data ("name")
+where not exists (
+        select *
+        from system_names
+        where data = system_names
+    );
+
+select *
+from system_names;
