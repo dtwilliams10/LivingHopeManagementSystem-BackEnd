f42033bb2141627a8bf4de7248996b7a2d083f15
Tyler Williams <david.williams110913@gmail.com>
2023-06-17 12:00:57 -0400

    Added test project

---
+++ tests/Services/SystemReportServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using LHMS.SystemReports.Services;
+using AutoFixture;
+using Moq;
+
+namespace SystemReportsTests.Services
+{
+	public class SystemReportServiceTests
+	{
+		[Fact]
+		public void Test1()
+		{
+			Assert.True(true);
+		}
+
+	}
+}
+
