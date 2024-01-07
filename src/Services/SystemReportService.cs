37c96ded08570ebe3556cd77879ee7f75940ff0f
Tyler Williams <david.williams110913@gmail.com>
2024-01-07 09:12:07 -0500

    Changed System Report Service to be asyncrhronous

--- src/Services/SystemReportService.cs
+++ src/Services/SystemReportService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using LHMS.SystemReports.Helpers;
 using LHMS.SystemReports.Models.SystemReport;
 using LHMS.SystemReports.Entities;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 using AutoMapper;
+using System.Threading.Tasks;
 
 namespace LHMS.SystemReports.Services
 {
@@ -18,9 +18,9 @@ namespace LHMS.SystemReports.Services
 
         SystemReportResponse GetByID(int id);
 
-        SystemReportResponse Create(SystemReportRequest model);
+        Task<SystemReportResponse> Create(SystemReportRequest model);
 
-        SystemReportResponse Update(SystemReportRequest model);
+        Task<SystemReportResponse> Update(SystemReportRequest model);
 
         void Delete(int id);
     }
@@ -29,27 +29,33 @@ namespace LHMS.SystemReports.Services
     {
         private readonly DatabaseContext _context;
         private readonly IMapper _mapper;
+        private readonly ISystemNameService _systemNameService;
+        private readonly ISystemReportStatusService _systemReportStatusService;
 
-        public SystemReportService(DatabaseContext context, IMapper mapper)
+        public SystemReportService(DatabaseContext context, IMapper mapper, ISystemNameService systemNameService, ISystemReportStatusService systemReportStatusService)
         {
             _context = context;
             _mapper = mapper;
+            _systemNameService = systemNameService;
+            _systemReportStatusService = systemReportStatusService;
         }
-        
-        public SystemReportResponse Create(SystemReportRequest systemReportRequest)
+
+        public async Task<SystemReportResponse> Create(SystemReportRequest systemReportRequest)
         {
             try
             {
                 var systemReport = _mapper.Map<SystemReport>(systemReportRequest);
                 //TODO: Add logic to pull logged in user and assign to Reporter spot.
-                /// Grab deserialized JWT and add the userID to reporterID. 
-                systemReport.ReporterId = 4;
+                //Grab deserialized JWT and add the userID to reporterID. 
+                systemReport.ReporterId = systemReportRequest.ReporterId.ToString();
                 systemReport.CreatedDate = NodaTime.Instant.FromDateTimeUtc(DateTime.UtcNow);
                 systemReport.UpdatedDate = NodaTime.Instant.FromDateTimeUtc(DateTime.UtcNow);
+                systemReport.SystemReportStatus = await _systemReportStatusService.GetSystemReportStatusById(systemReport.SystemReportStatusId);
                 if (systemReport.SystemReportStatusId == 0)
                     systemReport.SystemReportStatusId = 1;
-                _context.SystemReports.AddAsync(systemReport);
-                _context.SaveChanges();
+                systemReport.SystemName = await _systemNameService.GetSystemNameById(systemReport.SystemNameId);
+                await _context.SystemReports.AddAsync(systemReport);
+                await _context.SaveChangesAsync();
 
                 var response = _mapper.Map<SystemReportResponse>(systemReport);
                 return response;
@@ -84,17 +90,17 @@ namespace LHMS.SystemReports.Services
 
         public IQueryable<SystemReportResponse> GetAllSystemReports()
         {
-            var systemReports = _context.SystemReports.Where(s => s.SystemReportStatusId > 1).ToList();
-            
-            List<SystemReportResponse> response = new List<SystemReportResponse>();
-            foreach(SystemReport sr in systemReports)
+            var systemReports = _context.SystemReports.ToList();
+
+            List<SystemReportResponse> response = new();
+            foreach (SystemReport sr in systemReports)
             {
                 sr.SystemName = _context.SystemNames.FirstOrDefault(s => s.Id == sr.SystemNameId);
                 sr.SystemReportStatus = _context.SystemReportStatus.FirstOrDefault(s => s.Id == sr.SystemReportStatusId);
                 var mappedReport = _mapper.Map<SystemReportResponse>(sr);
                 response.Add(mappedReport);
             }
-            
+
             return response.AsQueryable();
         }
 
@@ -112,11 +118,34 @@ namespace LHMS.SystemReports.Services
             throw new System.NotImplementedException();
         }
 
-        public SystemReportResponse Update(SystemReportRequest systemReport)
+        public async Task<SystemReportResponse> Update(SystemReportRequest systemReportRequest)
         {
-            SystemReport systemReports = _context.SystemReports.First(s => s.Id == systemReport.Id);
+            try
+            {
+                SystemReport systemReport = await _context.SystemReports.FindAsync(systemReportRequest.Id);
+                if (systemReport is not null)
+                {
+                    systemReport.BarriersOrChallenges = systemReportRequest.BarriersOrChallenges;
+                    systemReport.CreativeIdeasAndEvaluations = systemReportRequest.CreativeIdeasAndEvaluations;
+                    systemReport.HowCanIHelpYou = systemReportRequest.HowCanIHelpYou;
+                    systemReport.PersonnelUpdates = systemReportRequest.PersonnelUpdates;
+                    systemReport.PersonalGrowthAndDevelopment = systemReportRequest.PersonalGrowthAndDevelopment;
+                    systemReport.SystemNameId = systemReportRequest.SystemName.Id;
+                    systemReport.SystemReportStatusId = systemReportRequest.SystemReportStatus.Id;
+                    systemReport.SystemUpdate = systemReportRequest.SystemUpdate;
+                    systemReport.UpdatedDate = NodaTime.Instant.FromDateTimeUtc(DateTime.UtcNow);
+                }
 
-            throw new System.NotImplementedException();
+                SystemReportResponse response = _mapper.Map<SystemReportResponse>(systemReport);
+                await _context.SaveChangesAsync();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error updating System Report with ID: {@id}", systemReportRequest.Id);
+                throw new AppException(ex.InnerException.ToString());
+            }
+
         }
     }
 }
