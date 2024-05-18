862f663ed07aa43488e0d2cfbb3ec483f726bdbc
Tyler Williams <david.williams110913@gmail.com>
2024-05-18 09:32:57 -0400

    Cleaning up SonarQube warnings.

--- src/Helpers/DatabaseContext.cs
+++ src/Helpers/DatabaseContext.cs
@@ -11,7 +11,7 @@ namespace LHMS.SystemReports.Helpers
         {
             Configuration = configuration;
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseNpgsql(Configuration.GetConnectionString("SystemReports"), o => o.UseNodaTime()).UseSnakeCaseNamingConvention();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql(Configuration.GetConnectionString("SystemReports"), o => o.UseNodaTime()).UseSnakeCaseNamingConvention();
         public DbSet<SystemReport> SystemReports { get; set; }
         public DbSet<SystemReportStatus> SystemReportStatus { get; set; }
         public DbSet<SystemName> SystemNames { get; set; }
